d273c1e5e4ca8c44c3d9c65978083c89b9785c96
Vadym <vadym.horval@gmail.com>
2021-08-20 14:51:34 +0300

    1st

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import render_template
+from flask import request
+from flask import redirect
+import csv
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    return app
+app = create_app()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == "POST":
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database!'
+    else:
+        return 'Something went wrong!!!'
+
+if __name__ == '__main__':
+    app.run()
